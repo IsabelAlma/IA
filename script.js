d62590bb458797f7d6cce875f24455334b125178
IsabelAlma <IsabelAlma@users.noreply.github.com>
2020-05-08 11:18:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-let submitButton = document.querySelector('#submit-button');
-function clickListener(event) {
-	event.preventDefault();
-	let emailInput = document.querySelector('#email');
-	let messageInput = document.querySelector('#message');
-
-	let emailText = emailInput.value;
-	let messageText = messageInput.value;
-
-	console.log('email:', emailText, 'message:', messageText);
+function emailValidate(email) {
+    if(email.includes('@')) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
+
+
+function clickListener(event) {
+    event.preventDefault();
+    let emailInput = document.querySelector('#email');
+    let messageInput = document.querySelector('#message');
+
+    let emailText = emailInput.value;
+    let messageText = messageInput.value;
+
+    	if(emailValidate(emailText) !== true) {
+		console.log('The email address must contain @');
+		return false;
+  }
+  console.log('Thanks for your message');
+}
+
+let submitButton = document.querySelector('#submit-button');
+
 submitButton.addEventListener('click', clickListener);
