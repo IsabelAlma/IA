db7011598a8242224f49d92e6103a65c35ea7c82
IsabelAlma <IsabelAlma@users.noreply.github.com>
2020-05-10 22:04:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let submitButton = document.querySelector('#submit-button');
 function emailValidate(email) {
     if(email.includes('@')) {
         return true;
@@ -6,8 +7,6 @@ function emailValidate(email) {
         return false;
     }
 }
-
-
 function clickListener(event) {
     event.preventDefault();
     let emailInput = document.querySelector('#email');
@@ -23,6 +22,17 @@ function clickListener(event) {
   console.log('Thanks for your message');
 }
 
-let submitButton = document.querySelector('#submit-button');
-
 submitButton.addEventListener('click', clickListener);
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+} 
